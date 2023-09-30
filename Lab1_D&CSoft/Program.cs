63f3173f1baf510ed882fdf0e7f128d8d6b26193
Acsious <ulchenko.f@mail.ru>
2023-09-30 13:23:19 +0300

    Удален ненужный комментарий

--- Lab1_D&CSoft/Program.cs
+++ Lab1_D&CSoft/Program.cs
@@ -5,18 +5,3 @@ crew.Elf.CreateWeapon().Hit();
 crew.Warrior.CreateWeapon().Hit();
 crew.Elf.CreateMovement().Move();
 crew.Warrior.CreateMovement().Move();
-
-//namespace Lab1DCSoft
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Crew crew = new Crew();
-//            crew.Elf.CreateWeapon().Hit();
-//            crew.Warrior.CreateWeapon().Hit();
-//            crew.Elf.CreateMovement().Move();
-//            crew.Warrior.CreateMovement().Move();
-//        }
-//    }
-//}
